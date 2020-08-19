4b28fb4ea4806c9a8e5875190f56fe2109406624
Maguod <mgod.bf@gmail.com>
2020-08-19 11:27:39 +0200

    Logout user on registred

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -73,6 +73,8 @@ class RegisterController extends Controller
     }
     protected function registered(Request $request, $user)
     {
-        $user->logout();
+        $this->guard()->logout();
+        return redirect()->route('login')
+            ->with('success', 'Я вас запомнил');
     }
 }
